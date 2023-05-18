e3c345a2b685c8bf31a31fb1e52b7d2b4d4fef9a
luisotavio2023 <luisogois@gmail.com>
2023-05-17 21:42:15 -0300

    Modificação da interface e adicionado arquivos novos

--- SiteSG/index.php
+++ SiteSG/index.php
@@ -5,23 +5,91 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" type="text/css" href="cssindex.css">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
   <body>
-  <nav class="navbar navbar-light bg-dark">
-  <a class="navbar-brand" href="#">
-    <img src="./images/logo.png" width="40" height="35" class="d-inline-block align-top" alt="">
-    Sigma City RP
-  </a>
-  <nav class="navbar navbar-light bg-dark">
-  <form class="form-inline">
-    <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar">
-    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
-  </form>
-</nav>
-</nav>
+  <div class="container">
+    <header class="d-flex flex-wrap d-flex w-100 align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+      <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+        <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
+      </a>
+
+      <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+        <li><a href="#" class="nav-link px-2 link-secondary">Início</a></li>
+        <li><a href="#" class="nav-link px-2 link-dark">Notícias</a></li>
+        <li><a href="#" class="nav-link px-2 link-dark">Discord</a></li>
+        <li><a href="#" class="nav-link px-2 link-dark">Reclame aqui</a></li>
+        <li><a href="#" class="nav-link px-2 link-dark">Tste</a></li>
+      </ul>
+
+      <div class="col-md-3 text-end">
+        <button type="button" class="btn btn-outline-primary me-2">Login</button>
+        <button type="button" class="btn btn-primary">Sign-up</button>
+      </div>
+    </header>
+    <div id="wrapper">
+   <div class="overlay"></div>
+    
+        <!-- Sidebar -->
+    <nav class="navbar navbar-inverse fixed-top" id="sidebar-wrapper" role="navigation">
+     <ul class="nav sidebar-nav">
+       <div class="sidebar-header">
+       <div class="sidebar-brand">
+         <a href="#">Sigma City</a></div></div>
+       <li><a href="#home">Home</a></li>
+       <li><a href="#about">Sobre</a></li>
+       <li><a href="#events">Eventos</a></li>
+       <li><a href="#team">Fale conosco</a></li>
+       <li class="dropdown">
+       <a href="#works" class="dropdown-toggle"  data-toggle="dropdown"> <span class="caret"></span></a>
+     <ul class="dropdown-menu animated fadeInLeft" role="menu">
+      <div class="dropdown-header">Painel de controle</div>
+      <li><a href="#pictures">Avisos</a></li>
+      <li><a href="#videos">Perfil</a></li>
+      <li><a href="#books"></a>Membros</li>
+      <li><a href="#art">Adicionais a venda</a></li>
+      <li><a href="#awards">Financeiro</a></li>
+      </ul>
+      </li>
+      <li><a href="#services"></a></li>
+      <li><a href="#contact">Youtubers do servidor</a></li>
+      <li><a href="#followme">Redes sociais</a></li>
+      </ul>
+  </nav>
+        <!-- /#sidebar-wrapper -->
+
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+            <button type="button" class="hamburger animated fadeInLeft is-closed" data-toggle="offcanvas">
+                <span class="hamb-top"></span>
+    			<span class="hamb-middle"></span>
+				<span class="hamb-bottom"></span>
+            </button>
+                </div>
+            </div>
+        </div>
+        <!-- /#page-content-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <footer class="text-center text-white fixed-bottom" style="background-color: #21081a;">
+  <!-- Grid container -->
+  <div class="container p-4"></div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2023 Copyright:
+    <a class="text-white" href="https://mdbootstrap.com/">Sigma City RP</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+  </div>
+
 
     
       
@@ -31,4 +99,5 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </body>
+  <script src="JavaScript.js"></script>
 </html>
--- SiteSG/painelloginreg.php
+++ SiteSG/painelloginreg.php
@@ -5,11 +5,55 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <style>
+        .bg-image {
+            background-image: url('images/fundolr.jpg');
+            background-size: cover;
+            background-position: center;
+            height: 100vh;
+        }
+
+        body, html {
+            height: 100%;
+        }
+
+    </style>
 
     <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="cssdosite.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
   <body>
+  <div class="bg-image">
+    <br><br><br><br>
+  <div class="container mt-5 p-0">
+        <div class="row justify-content-center d-flex align-items-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h2 class="card-title">Seja bem vindo(a)</h2>
+                        <form>
+                            <div class="form-group">
+                                <label for="username">Usuário</label>
+                                <input type="text" class="form-control" id="username" name="username" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="password">Senha</label>
+                                <input type="password" class="form-control" id="password" name="password" required>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Entrar</button>
+                        </form>
+                    </div>
+                    <div class="card-footer text-center">
+                        <a href="esqueci-senha.html">Esqueci minha senha</a> | <a href="registro.html">Registrar-se</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+  
+
       
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
