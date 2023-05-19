412d049d12abbfba4d4f3262d0b6611406cdbac4
luisotavio2023 <luisogois@gmail.com>
2023-05-19 15:13:16 -0300

    Adicionando a loja e alterando algumas linhas no index

--- SiteSG/index.php
+++ SiteSG/index.php
@@ -19,8 +19,8 @@
 
       <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
         <li><a href="#" class="nav-link px-2 link-secondary">Início</a></li>
-        <li><a href="#" class="nav-link px-2 link-dark">Notícias</a></li>
-        <li><a href="#" class="nav-link px-2 link-dark">Discord</a></li>
+        <li><a href="loja.php" class="nav-link px-2 link-dark">Loja do servidor</a></li>
+        <li><a href="https://discord.gg/eeqn7gJpTT" class="nav-link px-2 link-dark">Discord</a></li>
         <li><a href="#" class="nav-link px-2 link-dark">Regras servidor</a></li>
         <li><a href="#" class="nav-link px-2 link-dark">Equipe Staff</a></li>
       </ul>
@@ -49,7 +49,7 @@
       <div class="dropdown-header">Painel de controle</div>
       <li><a href="#pictures">Avisos</a></li>
       <li><a href="#videos">Perfil</a></li>
-      <li><a href="#books"></a>Membros</li>
+      <li><a href="#books">Membros</a></li>
       <li><a href="#art">Adicionais a venda</a></li>
       <li><a href="#awards">Financeiro</a></li>
       </ul>
